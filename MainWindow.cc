09b909732729fb1d74446c9d95e4e97e2eb80bf1
Chris Webster <cjw@ucar.edu>
2019-03-27 10:33:49 -0600

    petajoules host is now behind a router.
    Set hostname accordingly.

--- MainWindow.cc
+++ MainWindow.cc
@@ -163,7 +163,7 @@ void MainWindow::setupDatabase()
 */
     // List of remote sites that fill a calibration database.
     QStringList siteList;
-    siteList << "petajoules.raf-acrouter.eol.ucar.edu";
+    siteList << "raf-acrouter.eol.ucar.edu";	// Tech labstation.
     siteList << "hyper.raf-guest.ucar.edu";
     siteList << "hercules.raf-guest.ucar.edu";
 
